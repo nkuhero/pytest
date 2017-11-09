4174f705e89f9261889421ad7af2eddfcbe4d285
nkuhero <nkuhero@qq.com>
2017-11-09 15:44:51 +0800

    Update longestPalindrome_brute.py

--- longestPalindrome_brute.py
+++ longestPalindrome_brute.py
@@ -1,4 +1,4 @@
-#暴力穷举法
+#暴力穷举法 算法复杂度O(n*n*n)
 class Solution:
     def longestPalindrome(self, s):
         """
@@ -8,13 +8,14 @@ class Solution:
         longest = ""
         if len(s) == 1:
             return s
+        #穷举所有可能的子串
         for i in range(len(s)):
             for j in range(i+1, len(s)):
                 if self.isPalindrome(s[i:j+1]) and len(s[i:j+1]) > len(longest):
                     longest = s[i:j+1]
         return longest
 
-
+    #判断一个字符串是不是回文字符串
     def isPalindrome(self, s):
         l = len(s)
         if l % 2 == 1:
