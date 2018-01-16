ee05104bfbdd1599c699401ddc90c2e4f1cf103e
nkuhero <nkuhero@qq.com>
2018-01-16 16:44:48 +0800

    Update singleton.py

--- singleton.py
+++ singleton.py
@@ -16,15 +16,12 @@ print(A is B)
 
 
 class SingletonMetaClass(type):
-    def __init__(cls, *args, **kwargs):
-        cls._instance = None
-        super(SingletonMetaClass, cls).__init__(*args, **kwargs)
-
+    
     def __call__(cls, *args, **kwargs):
-        if cls._instance is None:
+        if not hasattr(cls, "_instance"):
             cls._instance = super(SingletonMetaClass, cls).__call__(*args, **kwargs)
         return cls._instance
-
+    
 class MyClass2(object):
     __metaclass__ = SingletonMetaClass
     pass
