34ee414847bd7a5513391771cb39ee517369d8c1
nkuhero <nkuhero@qq.com>
2018-01-24 10:37:26 +0800

    Update find_same_node.py

--- find_same_node.py
+++ find_same_node.py
@@ -1,31 +1,29 @@
-#交叉链表求交点
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.next = next
-        self.val = val
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+def node(l1, l2):
+    length1, length2 = 0, 0
+    # 求两个链表长度
+    while l1.next:
+        l1 = l1.next
+        length1 += 1
+    while l2.next:
+        l2 = l2.next
+        length2 += 1
+    # 长的链表先走
+    if length1 > length2:
+        for _ in range(length1 - length2):
+            l1 = l1.next
+    else:
+        for _ in range(length2 - length1):
+            l2 = l2.next
+    while l1 and l2:
+        if l1.next == l2.next:
+            return l1.next
+        else:
+            l1 = l1.next
+            l2 = l2.next
 
-    def __repr__(self):
-        return str(self.val)
 
-l1 = ListNode(1, ListNode(2, ListNode(3, ListNode(4))))
 
-l2 = ListNode(5, ListNode(3, ListNode(6, ListNode(8))))
-
-def find_same_node(l1, l2):
-    head1 = l1
-    while head1:
-        v1 = head1.val
-        print("v1=%s"%v1)
-        head2 = l2
-        while head2:
-            v2 = head2.val
-            print("v2=%s"%v2)
-            if v1 == v2:
-                return v1
-            else:
-                head2 = head2.next
-        head1 = head1.next
-    return None
-
-result = find_same_node(l1, l2)
-print(result)
